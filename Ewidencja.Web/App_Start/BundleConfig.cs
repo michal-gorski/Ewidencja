7486c21fb5c6d4d86711409cf88c3852978d4579
Baca <baca@pracownia.pl>
2013-07-29 22:27:56 +0200

    Changed html to use bootstrap, wip

--- Ewidencja.Web/App_Start/BundleConfig.cs
+++ Ewidencja.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace Ewidencja
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
             //           "~/Scripts/jquery-ui-{version}.js"));
@@ -18,41 +18,17 @@ namespace Ewidencja
             //           "~/Scripts/jquery.unobtrusive*",
             //            "~/Scripts/jquery.validate*"));
 
-            
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
-            
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapJS").Include("~/Scripts/bootstrap.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/ewidencjaScript").Include("~/Scripts/panel.js"));
 
 
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/normalize.css",
-                "~/Content/common.css", 
-                "~/Content/bootstrap.css",
-                
-                //"~/Content/simpliq.css",
-                //"~/Content/simpliq-responsive.css",
-
-                "~/Content/font-awesome.css"));
-
-            bundles.Add(new Bundle("~/Content/less").IncludeDirectory("~/Content", "*.less", false));
-
-
-            /*bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
-             */ 
+            bundles.Add(new Bundle("~/bundles/bootstrapCSS").Include(new []{"~/Content/bootstrap/bootstrap.css"}));
+            bundles.Add(new StyleBundle("~/bundles/fontAwesome").Include(new[] {"~/Content/font-awesome.css"} ));
+            bundles.Add(new Bundle("~/bundles/styles").IncludeDirectory("~/Content", "*.less", false));
+           
         }
     }
 }

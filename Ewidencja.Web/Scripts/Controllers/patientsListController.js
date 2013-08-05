ec8f94a530eef6674f24d498ba795898890dbbb6
Baca <baca@pracownia.pl>
2013-08-05 11:45:41 +0200

    Patients List WIP

--- Ewidencja.Web/Scripts/Controllers/patientsListController.js
+++ Ewidencja.Web/Scripts/Controllers/patientsListController.js
@@ -1,14 +1,12 @@
 ﻿function PatientsListController($scope, PatientsList) {
+
     $scope.patients = PatientsList.GetPatients();
 
-    $scope.AddPatient = function ()
-    {
-        $scope.patients.push({
-            name: "Anna",
-            surname: "Kowalska",
-            address: "Słowackiego 10, Gdańsk"
-        });
-    };
+
+
+
+
+    
 
 
 }

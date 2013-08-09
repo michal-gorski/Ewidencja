588bd58a58cbf1aa1981c26c597b56ed29825140
Baca <baca@pracownia.pl>
2013-08-09 17:21:25 +0200

    disable bundling

--- Ewidencja.Web/App_Start/BundleConfig.cs
+++ Ewidencja.Web/App_Start/BundleConfig.cs
@@ -8,17 +8,13 @@ namespace Ewidencja
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            
+            BundleTable.EnableOptimizations = false;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/lib/jquery-{version}.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            //           "~/Scripts/jquery-ui-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //           "~/Scripts/jquery.unobtrusive*",
-            //            "~/Scripts/jquery.validate*"));
-
-
+            
             bundles.Add(new ScriptBundle("~/bundles/bootstrapJS").Include("~/Scripts/lib/bootstrap.js"));
             bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/lib/angular.js"));
 

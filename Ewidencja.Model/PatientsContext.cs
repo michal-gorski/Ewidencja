48cb9f199e8aafd4fcdb81f544380edce50b9ecc
Baca <baca@pracownia.pl>
2013-08-09 13:14:10 +0200

    Changes to connection strings

--- Ewidencja.Model/PatientsContext.cs
+++ Ewidencja.Model/PatientsContext.cs
@@ -5,12 +5,19 @@ namespace Ewidencja.Model
 {
     internal class PatientsContext : DbContext
     {
-        public string LegacyDBPath { get; set; }
-        public DbSet<Patient> Patients { get; set; }
-
         public PatientsContext()
+            : base("Ewidencja.PatientsContext")
         {
             Database.SetInitializer(new PatientsContextInitializer());
         }
+
+        public PatientsContext(string connectionStringName)
+            : base(connectionStringName)
+        {
+        }
+
+        public string LegacyDBPath { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        
     }
 }
